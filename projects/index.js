0ccdff33f766bd91d3ae8896a50f27e3ae9ad0b3
김효준 <wangbc2@gmail.com>
2019-04-15 15:38:11 +0900

    1-8

--- projects/index.js
+++ projects/index.js
@@ -1,44 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import './App.css';
 
 const Links = () => (
     <nav>
-        <Link to="home">Home</Link>
-        <Link to="menu">Menu</Link>
+        <Link to="/">Home</Link>
+        <Link to="/old">Old</Link>
+        <Link to="/new">New</Link>
     </nav>
 );
 
-const Home = () => (
-    <h1>Home</h1>
-);
-
-const Menu = (match) => {
-
-    return (
-        <div>
-            <Link to="/menu/food">Food</Link>
-            <Link to="/menu/drink">Drinks</Link>
-            <Link to="/menu/sides">Sides</Link>
-        </div>
-    );
-};
 
 const App = () => (
 
     <Router>
         <div>
             <Links />
-            <Route path="/" component={Home} />
-            <Route path="/menu" component={Menu} />
-            <Route
-                path="/menu/:subitem"
+            <Route exact path="/"
+                render={(match) => <h1>Home</h1>}    
+            />
+            <Route path="/new/:id"
+                render={(match) => (
+                    <h1>New: {match.match.params.id}</h1>
+                )}    
+            />
+            <Route path="/old/:id"
                 render={(match) => {
-                    return (
-                        <h2>{match.match.params.subitem}</h2>
-                    );
-                }}
+                    console.log(match)
+
+                    return <Redirect from="old" to={"/new/" + match.match.params.id} />
+                }}    
             />
         </div>
     </Router>
